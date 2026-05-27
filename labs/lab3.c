2353635c0b494e6af6e9468db65f601ffc42fe69
RishabhThakur06 <733.rishabh.thakur@gmail.com>
2026-05-27 09:57:26 +0530

    Fixed lab 3 code

--- labs/lab3.c
+++ labs/lab3.c
@@ -1,65 +1,71 @@
 #include <stdio.h>
 
-struct Process
-{
-    int pid;
-    int burstTime;
-    int waitingTime;
-    int turnaroundTime;
-};
+void fcfs(int processes[], int n, int arrival_time[], int burst_time[]) {
+    int waiting_time[n], turnaround_time[n], C_Time[n];
+    int total_waiting_time = 0, total_turnaround_time = 0;
 
-int main()
-{
+    for (int i = 0; i < n; i++) {
+        if (i == 0)
+            C_Time[i] = arrival_time[i] + burst_time[i];
+        else
+            C_Time[i] = C_Time[i - 1] + burst_time[i];
+    }
+
+    // Calculate turnaround time for each process
+    for (int i = 0; i < n; i++) {
+        turnaround_time[i] = C_Time[i] - arrival_time[i];
+    }
+
+    // Calculate waiting time for each process
+    for (int i = 0; i < n; i++) {
+        waiting_time[i] = turnaround_time[i] - burst_time[i];
+        total_waiting_time += waiting_time[i];
+        total_turnaround_time += turnaround_time[i];
+    }
+
+    // Print process details
+    printf("\nProcess\tArrival Time\tBurst Time\tCompletion Time\tWaiting Time\tTurnaround Time\n");
+
+    for (int i = 0; i < n; i++) {
+        printf("P%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n",
+               processes[i],
+               arrival_time[i],
+               burst_time[i],
+               C_Time[i],
+               waiting_time[i],
+               turnaround_time[i]);
+    }
+
+    // Print average waiting time and average turnaround time
+    printf("\nAverage Waiting Time = %.2f",
+           (float)total_waiting_time / n);
+
+    printf("\nAverage Turnaround Time = %.2f\n",
+           (float)total_turnaround_time / n);
+}
+
+int main() {
     int n;
-    int totalWaitingTime = 0;
-    int totalTurnaroundTime = 0;
 
-    printf("Enter number of processes: ");
+    // Input number of processes
+    printf("Enter the number of processes: ");
     scanf("%d", &n);
 
-    struct Process p[n];
+    int processes[n], arrival_time[n], burst_time[n];
 
-    for (int i = 0; i < n; i++)
-    {
-        p[i].pid = i + 1;
+    // Input arrival time and burst time for each process
+    for (int i = 0; i < n; i++) {
+        printf("Enter arrival time for process %d: ", i + 1);
+        scanf("%d", &arrival_time[i]);
 
-        printf("Enter burst time for Process %d: ", p[i].pid);
-        scanf("%d", &p[i].burstTime);
+        printf("Enter burst time for process %d: ", i + 1);
+        scanf("%d", &burst_time[i]);
+
+        processes[i] = i + 1;
     }
 
-    p[0].waitingTime = 0;
-
-    for (int i = 1; i < n; i++)
-    {
-        p[i].waitingTime = p[i - 1].waitingTime + p[i - 1].burstTime;
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        p[i].turnaroundTime = p[i].waitingTime + p[i].burstTime;
-
-        totalWaitingTime += p[i].waitingTime;
-        totalTurnaroundTime += p[i].turnaroundTime;
-    }
-
-    printf("\nFCFS Scheduling\n");
-
-    printf("\nPID\tBurst Time\tWaiting Time\tTurnaround Time\n");
-
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d\t%d\t\t%d\t\t%d\n",
-               p[i].pid,
-               p[i].burstTime,
-               p[i].waitingTime,
-               p[i].turnaroundTime);
-    }
-
-    printf("\nAverage Waiting Time = %.2f\n",
-           (float)totalWaitingTime / n);
-
-    printf("Average Turnaround Time = %.2f\n",
-           (float)totalTurnaroundTime / n);
+    // Call FCFS scheduling function
+    fcfs(processes, n, arrival_time, burst_time);
 
     return 0;
 }
